90ad20d99e0ccabf371b6b4e1d68845b9a70a4b8
Dheeraj TP <45311336+dheerajtp@users.noreply.github.com>
2022-03-13 18:55:42 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,13 +6,13 @@ function Footer() {
   return (
     <footer className="text-gray-600 body-font">
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-        <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+        <span className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
           <img
             src={pic}
             className="w-10 h-10  p-2  rounded-full"
             alt="footer"
           />
-        </a>
+        </span>
         <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
           © {year} dheeraj
         </p>
